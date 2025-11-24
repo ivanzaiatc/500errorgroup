ff84a1565ed693de1a8d00ddb1c93c20913fcba4
Ivan Zaiatc <zaiatcivan@gmail.com>
2025-11-24 12:44:11 +0200

    Updates

--- _next/static/wJ5a8lvZvCtmDjgaRNIyE/_buildManifest.js
+++ _next/static/wJ5a8lvZvCtmDjgaRNIyE/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/500errorgroup//_next/:path+",
+        "destination": "/500errorgroup/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
