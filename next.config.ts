a83a73bf85a67a4f58c103029f5bcc8f040a77c4
Ivan Zaiatc <zaiatcivan@gmail.com>
2025-11-24 12:16:46 +0200

    change configs

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true, 
   },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
+  assetPrefix: '',
+  basePath: '',
   output: 'export'
 };
 
