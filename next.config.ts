d297b2d3c769ed364cc0946c13a0afff7d95c51d
Ivan Zaiatc <zaiatcivan@gmail.com>
2025-11-24 12:43:32 +0200

    change path

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, 
+    unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: '',
-  basePath: '',
+  assetPrefix: isProd ? '/500errorgroup/' : '',
+  basePath: isProd ? '/500errorgroup' : '',
   output: 'export'
 };
 
